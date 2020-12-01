ffac7faef37d794d9b9ad3c5c1de2ddf9292a040
kchia <kchia87@gmail.com>
2020-12-01 13:58:04 -1000

    add aggregate database queries

--- src/products/products.controller.js
+++ src/products/products.controller.js
@@ -14,13 +14,69 @@ async function productExists(req, res, next) {
   next();
 }
 
+function queryParametersAreValid(req, res, next) {
+  const validQueryParameters = {
+    get_out_of_stock_count: ["1"],
+    get_prices_summary_stats: ["1"],
+    get_total_weight: ["1"],
+  };
+
+  for (const [key, value] of Object.entries(req.query)) {
+    if (
+      !validQueryParameters[key] ||
+      !validQueryParameters[key].includes(value)
+    ) {
+      return next({
+        status: 400,
+        message: `${key}=${value} is not a valid query parameter.`,
+      });
+    }
+  }
+
+  const {
+    get_out_of_stock_count: getOutOfStockCount = "",
+    get_prices_summary_stats: getPricesSummaryStats = "",
+    get_total_weight: getTotalWeight = "",
+  } = req.query;
+
+  res.locals.query = {
+    getOutOfStockCount,
+    getPricesSummaryStats,
+    getTotalWeight,
+  };
+
+  next();
+}
+
 function read(req, res, next) {
   const { product } = res.locals;
   res.json({ data: product });
 }
 
 async function list(req, res, next) {
-  const products = await ProductsService.getAllProducts();
+  const { query } = res.locals;
+  const { getOutOfStockCount, getPricesSummaryStats, getTotalWeight } = query;
+
+  let products;
+
+  // /products
+  if (!getOutOfStockCount && !getPricesSummaryStats && !getTotalWeight) {
+    products = await ProductsService.getAllProducts();
+  }
+  // /products?get_out_of_stock_count=1
+  if (getOutOfStockCount === "1") {
+    products = await ProductsService.getOutOfStockProductsCount();
+  }
+
+  // /products?get_prices_summary_stats=1
+  if (getPricesSummaryStats === "1") {
+    products = await ProductsService.getMinMaxAveragePricesOfProductsBySupplier();
+  }
+
+  // /products?get_total_weight=1
+  if (getTotalWeight === "1") {
+    products = await ProductsService.getTotalWeightOfEachProduct();
+  }
   res.json({ data: products });
 }
 
--- src/products/products.service.js
+++ src/products/products.service.js
@@ -7,7 +7,38 @@ const getAllProducts = () => products.select("*");
 const getProductById = (productId) =>
   products.select("*").where({ product_id: productId }).first();
 
+const getOutOfStockProductsCount = () =>
+  products
+    .select("product_quantity_in_stock as out_of_stock")
+    .count("product_id")
+    .where({ product_quantity_in_stock: 0 })
+    .groupBy("out_of_stock");
+
+const getMinMaxAveragePricesOfProductsBySupplier = () =>
+  products
+    .select("supplier_id")
+    .min("product_price")
+    .max("product_price")
+    .avg("product_price")
+    .groupBy("supplier_id")
+    .orderBy("supplier_id");
+
+const getTotalWeightOfEachProduct = () =>
+  products
+    .select(
+      "product_sku",
+      "product_title",
+      knex.raw(
+        "sum(product_weight_in_lbs * product_quantity_in_stock) as total_weight_in_lbs"
+      )
+    )
+    .groupBy("product_title", "product_sku")
+    .orderBy("total_weight_in_lbs");
+
 module.exports = {
   getAllProducts,
   getProductById,
+  getOutOfStockProductsCount,
+  getMinMaxAveragePricesOfProductsBySupplier,
+  getTotalWeightOfEachProduct,
 };
