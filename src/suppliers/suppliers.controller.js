716cd15274d337b9db1f1b520bdac4d13843f8f5
kchia <kchia87@gmail.com>
2021-01-27 16:41:49 -1000

    feat: update create function

--- src/suppliers/suppliers.controller.js
+++ src/suppliers/suppliers.controller.js
@@ -42,59 +42,7 @@ function bodyDataHas(propertyName) {
 const hasSupplierName = bodyDataHas("supplier_name");
 const hasSupplierEmail = bodyDataHas("supplier_email");
 
-function supplierExists(req, res, next) {
-  const error = { status: 404, message: `Supplier cannot be found.` };
-  const { supplierId } = req.params;
-  if (!supplierId) return next(error);
-
-  SuppliersService.getSupplierById(supplierId).then(supplier => {
-    if (!supplier) return next(error);
-    res.locals.supplier = supplier;
-    next();
-  });
-}
-
-function hasValidFields(req, res, next) {
-  const { data = {} } = req.body;
-  const validFields = new Set([
-    "supplier_name",
-    "supplier_address_line_1",
-    "supplier_address_line_2",
-    "supplier_city",
-    "supplier_state",
-    "supplier_zip",
-    "supplier_phone",
-    "supplier_email",
-    "supplier_notes",
-    "supplier_type_of_goods",
-  ]);
-
-  const invalidFields = Object.keys(data).filter(
-    field => !validFields.has(field)
-  );
-
-  if (invalidFields.length)
-    return next({
-      status: 400,
-      message: `Invalid field(s): ${invalidFields.join(", ")}`,
-    });
-  next();
-}
-
-function bodyDataHas(propertyName) {
-  return function (req, res, next) {
-    const { data = {} } = req.body;
-    if (data[propertyName]) {
-      return next();
-    }
-    next({ status: 400, message: `Supplier must include a ${propertyName}` });
-  };
-}
-
-const hasSupplierName = bodyDataHas("supplier_name");
-const hasSupplierEmail = bodyDataHas("supplier_email");
-
-function create(req, res, next) {
+async function create(req, res, next) {
   const newSupplier = ({
     supplier_name,
     supplier_address_line_1,
@@ -108,9 +56,19 @@ function create(req, res, next) {
     supplier_type_of_goods,
   } = req.body.data);
 
-  SuppliersService.createSupplier(newSupplier).then(createdSupplier =>
-    res.status(201).json({ data: createdSupplier })
-  );
+  let createdSupplier = await SuppliersService.createSupplier(newSupplier);
+  res.status(201).json({ data: createdSupplier });
+}
+
+async function supplierExists(req, res, next) {
+  const error = { status: 404, message: `Supplier cannot be found.` };
+  const { supplierId } = req.params;
+  if (!supplierId) return next(error);
+
+  let supplier = await SuppliersService.getSupplierById(supplierId);
+  if (!supplier) return next(error);
+  res.locals.supplier = supplier;
+  next();
 }
 
 async function update(req, res, next) {
