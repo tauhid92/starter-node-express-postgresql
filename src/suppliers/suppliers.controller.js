c2423b6bf76df4bbb719d0866878f68344a787fc
Md Tauhidul Islam <tauhidul2011@gmail.com>
2021-03-10 23:58:02 -0600

    first commit after adding everything

--- src/suppliers/suppliers.controller.js
+++ src/suppliers/suppliers.controller.js
@@ -1,17 +1,97 @@
-async function create(req, res, next) {
-  res.status(201).json({ data: { supplier_name: "new supplier" } });
+const SuppliersService = require("./suppliers.service.js");
+
+function hasValidFields(req, res, next) {
+  const { data = {} } = req.body;
+  const validFields = new Set([
+    "supplier_name",
+    "supplier_address_line_1",
+    "supplier_address_line_2",
+    "supplier_city",
+    "supplier_state",
+    "supplier_zip",
+    "supplier_phone",
+    "supplier_email",
+    "supplier_notes",
+    "supplier_type_of_goods",
+  ]);
+
+  const invalidFields = Object.keys(data).filter(
+    field => !validFields.has(field)
+  );
+
+  if (invalidFields.length)
+    return next({
+      status: 400,
+      message: `Invalid field(s): ${invalidFields.join(", ")}`,
+    });
+  next();
 }
 
-async function update(req, res, next) {
-  res.json({ data: { supplier_name: "updated supplier" } });
+function bodyDataHas(propertyName) {
+  return function (req, res, next) {
+    const { data = {} } = req.body;
+    if (data[propertyName]) {
+      return next();
+    }
+    next({ status: 400, message: `Supplier must include a ${propertyName}` });
+  };
 }
 
-async function destroy(req, res, next) {
-  res.sendStatus(204);
+const hasSupplierName = bodyDataHas("supplier_name");
+const hasSupplierEmail = bodyDataHas("supplier_email");
+
+function create(req, res, next) {
+  const newSupplier = ({
+    supplier_name,
+    supplier_address_line_1,
+    supplier_address_line_2,
+    supplier_city,
+    supplier_state,
+    supplier_zip,
+    supplier_phone,
+    supplier_email,
+    supplier_notes,
+    supplier_type_of_goods,
+  } = req.body.data);
+
+  SuppliersService.createSupplier(newSupplier).then(createdSupplier =>
+    res.status(201).json({ data: createdSupplier })
+  );
+}
+
+function supplierExists(req, res, next) {
+  const error = { status: 404, message: `Supplier cannot be found.` };
+  const { supplierId } = req.params;
+  if (!supplierId) return next(error);
+
+  SuppliersService.getSupplierById(supplierId).then(supplier => {
+    if (!supplier) return next(error);
+    res.locals.supplier = supplier;
+    next();
+  });
+}
+
+function update(req, res, next) {
+  const {
+    supplier: { supplier_id: supplierId, ...supplier },
+  } = res.locals;
+  const updatedSupplier = { ...supplier, ...req.body.data };
+
+  SuppliersService.updateSupplierById(
+    supplierId,
+    updatedSupplier
+  ).then(updatedSupplierRes => res.json({ data: updatedSupplierRes }));
+}
+
+function destroy(req, res, next) {
+  const { supplier } = res.locals;
+  SuppliersService.deleteSupplierById(supplier.supplier_id).then(() =>
+    res.sendStatus(204)
+  );
 }
 
 module.exports = {
-  create: [create],
-  update: [update],
-  destroy: [destroy],
+  create: [hasValidFields, hasSupplierName, hasSupplierEmail, create],
+  update: [supplierExists, update],
+  destroy: [supplierExists, destroy],
 };
